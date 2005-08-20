fd8db8385db5b54da5bb8c21763b4c4b003df5e3
jpetso <jpetso@e3de64f1-c3f7-0310-8e5c-d6e2c55166d5>
2005-08-20 20:00:37 +0000

    The big libqortage (now libpakt) refactoring: upload forgotten file

---
+++ trunk/pakoo/src/libqortage/core/fileloaderbase.cpp
@@ -0,0 +1,121 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Jakob Petsovits                                 *
+ *   jpetso@gmx.at                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "fileloaderbase.h"
+
+#include "../core/packagelist.h"
+#include "../core/dependatom.h"
+
+#include <qfile.h>
+#include <qtextstream.h>
+
+
+namespace libpakt {
+
+/**
+ * Initialize this object.
+ */
+FileLoaderBase::FileLoaderBase()
+{
+	filename = QString::null;
+}
+
+
+/**
+ * Set the name of the file that is used for loading and saving
+ * the package list.
+ */
+void FileLoaderBase::setFileName( const QString& filename )
+{
+	this->filename = filename;
+}
+
+/**
+ * Called by perform() every time a line has been read and before
+ * processLine() is called.
+ *
+ * If this function returns false, processLine() will not be called
+ * and perform() goes on with the next line in the file.
+ * It can be overloaded in order to skip comment lines. By default, this
+ * function returns false for empty lines and true for all other ones.
+ */
+bool FileLoaderBase::isLineProcessed( const QString& line )
+{
+	if( line.isEmpty() )
+		return false;
+	else
+		return true;
+}
+
+/**
+ * Scan and process the file. This function calls several virtual functions
+ * which can be overloaded by derived classes.
+ */
+IJob::JobResult FileLoaderBase::performThread()
+{
+	// Check if the file has been defined
+	if( filename.isNull() ) {
+		emit debugOutput(
+			"Didn't start the file loader because "
+			"the file name has not been set"
+		);
+		return Failure;
+	}
+
+	if( check() == false )
+		return Failure;
+
+	// Open the file for reading
+	QFile file( filename );
+
+	if( !file.open( IO_ReadOnly ) ) {
+		emit debugOutput(
+			QString( "Couldn't open %1 for reading" )
+				.arg( filename )
+		);
+		return Failure;
+	}
+
+	QString line;
+	QTextStream stream( &file );
+
+	if( init() == false )
+		return Failure;
+
+	// Process each line
+	while ( !stream.atEnd() )
+	{
+		line = stream.readLine();
+
+		if( isLineProcessed(line) == false )
+			continue;
+
+		processLine(line);
+
+		if( aborting() == true )
+			break;
+	}
+
+	file.close();
+
+	return finish();
+}
+
+} // namespace
