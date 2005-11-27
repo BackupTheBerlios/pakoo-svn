fc9992592ff794c87d428c4cc85dda4338b763e3
anmaster <anmaster@e3de64f1-c3f7-0310-8e5c-d6e2c55166d5>
2005-11-27 15:19:12 +0000

    Added an draft for a syncprocess.h. I hope it was in the right place. Added bug #005626 to the TODO.

---
+++ trunk/pakoo/src/libpakt/portage/installer/syncprocess.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Jakob Petsovits                                 *
+ *   jpetso@gmx.at                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "syncprocess.h"
+
+#include <qstringlist.h>
+
+namespace libpakt {
+
+/**
+ * Initialize this object.
+ * @see QObject::QObject(QObject*, const char*)
+ */
+SyncProcess::SyncProcess( QObject* parent, const char* name )
+	: ProcessJob( parent, name )
+{
+}
+
+/**
+ * Destruct this object.
+ */
+SyncProcess::~SyncProcess()
+{
+}
+
+ProcessJob::ProcessType SyncProcess::processType()
+{
+	return LocalProcess;
+}
+
+QStringList SyncProcess::processNameAndArguments( )
+{
+	QStringList list;
+	//FIXME: Big problems: How should the user enter the password?
+	//list << "su" << "-c" << "emerge --sync";
+	//NOTE: Syncing is a hard one to debug:
+	//      * The portage tree is only updated two times per hour.
+	//      * You may be blocked for abuse...
+	//FIXME: Until the above fixme is fixed:
+	list << "emerge" << "--sync";
+	return list;
+}
+
+} // namespace
