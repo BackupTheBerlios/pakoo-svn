a0ea7a6be43cc03257f97b072e9db25768699a83
anmaster <anmaster@e3de64f1-c3f7-0310-8e5c-d6e2c55166d5>
2005-12-22 18:58:31 +0000

    A few changes in the docs.

--- trunk/pakoo/src/libpakt/portage/installer/syncprocess.h
+++ trunk/pakoo/src/libpakt/portage/installer/syncprocess.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Jakob Petsovits                                 *
- *   jpetso@gmx.at                                                         *
+ *   Copyright (C) 2005 by Arvid Norlander                                 *
+ *   anmaster@users.berlios.de                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
